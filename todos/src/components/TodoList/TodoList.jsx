e64fe0a5ba973fccad87c2112cb8e94c5c4aa8e9
HYE <jelly12paw@gmail.com>
2023-02-21 17:19:54 +0900

    chore: TodoList와 Todo components 연결

--- todos/src/components/TodoList/TodoList.jsx
+++ todos/src/components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Adds from '../Adds/Adds';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -7,11 +8,13 @@ export default function TodoList() {
         // Add된 todo를 todos 리스트에 추가
         setTodos([...todos, todo])
     };
+    // status값이 변경된 id의 값을 찾아 status값을 변경
+    const handleUpdate = (updated) => setTodos(todos.map((t) => t.id === updated.id ? updated : t));
     return (
         <section>
             <ul>
                 {todos.map((item) => (
-                    <li key={item.id}>{item.text}</li>
+                    <Todo key={item.id} todo={item} onUpdate={handleUpdate} />
                 ))}
             </ul>
             <Adds onAdd={handleAdd}/>
