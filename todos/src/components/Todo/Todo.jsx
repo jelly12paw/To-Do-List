bc071e326422f200257af66922723b4c933e2643
HYE <jelly12paw@gmail.com>
2023-02-21 17:13:53 +0900

    chore: Todo checkbox로 status값 변경 기능 생성

--- todos/src/components/Todo/Todo.jsx
+++ todos/src/components/Todo/Todo.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default function Todo({todo}) {
+export default function Todo({todo, onUpdate}) {
     const [text, status] = todo;
+    const handleChange = (e) => {
+        // 체크박스가 선택되면 상태값은 done
+        status = e.target.checked ? 'done' : 'active';
+        // todo의 status값을 변경
+        onUpdate({...todo, status})
+    };
     return (
         <li>
-            <input type="checkbox" name="" id="statuscheck" />
+            <input type="checkbox" id="statuscheck" checked={status === 'done'} onChange={handleChange} />
             <label htmlFor="statuscheck">{text}</label>
         </li>
     );
