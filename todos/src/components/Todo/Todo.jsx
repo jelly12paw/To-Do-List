37e20f64d0d83eceeb65d2a126caeefa37ee440f
HYE <jelly12paw@gmail.com>
2023-02-21 22:07:48 +0900

    chore: Todo 체크박스 오류해결

--- todos/src/components/Todo/Todo.jsx
+++ todos/src/components/Todo/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {FaTrashAlt} from 'react-icons/fa';
 
 export default function Todo({todo, onUpdate, onDelete}) {
-    const {text, status} = todo;
+    const {id, text, status} = todo;
     const handleChange = (e) => {
         // 체크박스가 선택되면 상태값은 done
         const status = e.target.checked ? 'done' : 'active';
@@ -12,8 +12,8 @@ export default function Todo({todo, onUpdate, onDelete}) {
     const handleDelete = () => onDelete(todo);
     return (
         <li>
-            <input type="checkbox" id="statuscheck" checked={status === 'done'} onChange={handleChange} />
-            <label htmlFor="statuscheck">{text}</label>
+            <input type="checkbox" id={id} checked={status === 'done'} onChange={handleChange} />
+            <label htmlFor={id}>{text}</label>
             <button onClick={handleDelete}><FaTrashAlt/></button>
         </li>
     );
