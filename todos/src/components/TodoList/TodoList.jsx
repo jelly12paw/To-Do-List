818fdf74d51c893b5f6eb9c70277c346b5a137bc
HYE <jelly12paw@gmail.com>
2023-02-23 14:28:14 +0900

    chore: TodoList 로컬 스토리지에 값 저장

--- todos/src/components/TodoList/TodoList.jsx
+++ todos/src/components/TodoList/TodoList.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Adds from '../Adds/Adds';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css'
 
 export default function TodoList({filter}) {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(readTodosFromLocalStroage);
     const handleAdd = (todo) => {
         // Add된 todo를 todos 리스트에 추가
         setTodos([...todos, todo])
@@ -13,6 +13,10 @@ export default function TodoList({filter}) {
     const handleUpdate = (updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
     // todo값 삭제
     const handleDelete = (deleted) => setTodos(todos.filter((t) => (t.id !== deleted.id)))
+    // 로컬스토리지에 todo값을 json으로 변환하여 저장 (새로고침해도 사라지지않음)
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);    
     // status값에 따라 해당 값만 보여주기
     const filtered = getFilteredItem(todos, filter);
     return (
@@ -25,6 +29,12 @@ export default function TodoList({filter}) {
             <Adds onAdd={handleAdd}/>
         </section>
     );
+}
+
+function readTodosFromLocalStroage() {
+    const todos = localStorage.getItem('todos');
+    // todos를 읽어왔을 때 비어있지 않으면 json으로 변환
+    return todos ? JSON.parse(todos) : [];
 }
 
 function getFilteredItem(todos, filter) {
