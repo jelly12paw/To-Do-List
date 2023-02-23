795ace8b358dc59085d08911102cd2a13e6598f0
HYE <jelly12paw@gmail.com>
2023-02-23 14:17:18 +0900

    style: Todo component

--- todos/src/components/Todo/Todo.jsx
+++ todos/src/components/Todo/Todo.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaTrashAlt} from 'react-icons/fa';
-import styles from './Todo.module.css'
+import {AiOutlineClockCircle, AiOutlineCheck} from 'react-icons/ai';
+import styles from './Todo.module.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Todo({todo, onUpdate, onDelete}) {
     const {id, text, status} = todo;
@@ -11,12 +14,24 @@ export default function Todo({todo, onUpdate, onDelete}) {
         onUpdate({...todo, status})
     };
     const handleDelete = () => onDelete(todo);
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
         <li className={styles.todo}>
             <input className={styles.checkbox} type="checkbox" id={id} checked={status === 'done'} onChange={handleChange} />
-            <label className={styles.text} htmlFor={id}>{text}</label>
+            {/* <div className={styles.checkicon}><AiOutlineCheck/></div> */}
+            {/* <span className={styles.checkmark}></span> */}
+            <div>
+                <label className={styles.text} htmlFor={id}>{text}</label>
+                <div className={styles.clock}>
+                    <AiOutlineClockCircle/>
+                    <span className={styles.title}>DUE DATE</span>
+                    <span>
+                        <DatePicker className={styles.datepicker} selected={startDate} dateFormat="yyyy-MM-dd" onChange={(date) => setStartDate(date)} />
+                    </span>
+                </div>
+            </div>
             <div className={styles.icon}><button onClick={handleDelete}><FaTrashAlt/></button></div>
         </li>
     );
 }
-
